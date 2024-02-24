60f82b836bea854cfafd41a6b5660f04641bc373
AvrilHunter <153916526+AvrilHunter@users.noreply.github.com>
2024-02-24 08:58:24 +0000

    Update javascriptTestDoc.js

--- javascriptTestDoc.js
+++ javascriptTestDoc.js
@@ -1,3 +1,13 @@
 console.log("Editing this to test the autosave")
 const newConst = [1,4,3]
 console.log(newConst)
+
+
+function thisIsAFunction() {
+console.log("RainbowRainbowRainbow")
+console.log("SunshineSunshineSunshine")
+}
+
+const addData = [9,10,6,4]
+const longer = addData.push(5,6)
+
